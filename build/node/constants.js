844fb58fec8f603abd0ae08f3084ed230ee1f888
Clement Balestrat <clement.balestrat@gmail.com>
2021-07-21 21:32:02 +1000

    new build

--- build/node/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ERRORS = void 0;
-exports.ERRORS = {
-    badNetworkArg: 'unsupported network or network id passed. Please check SynthetixJS.supportedNetworks for a list of supported networks and ids',
-    noMatch: function (type, value) { return "no contracts match " + type + ": " + value; }
-};
--- build/node/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Config, CurrencyKey, CurrencyCategory, Network, NetworkId, Target, TargetsRecord, ContractsMap, SynthetixJS, Synth, Token } from './types';
-import { Synths } from '../generated/mainnet';
-declare const synthetix: ({ networkId, network, signer, provider }: Config) => SynthetixJS;
-export { synthetix, Network, NetworkId, Synths, CurrencyCategory };
-export type { Config, CurrencyKey, Target, TargetsRecord, ContractsMap, SynthetixJS, Synth, Token };
-export default synthetix;
